0c4f838fcbbccdbbf28788d55e96443f9fc93824
Amit Ramrakhyani <amit.rce21@sot.pdpu.ac.in>
2022-10-08 19:07:53 +0530

    User Login added

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    path('login/', views.loginPage, name="login"),
+
     path('', views.home, name='home'),
     # path('room/', views.room, name='room'),
     
--- base/views.py
+++ base/views.py
@@ -1,10 +1,34 @@
 import imp
 from multiprocessing import context
 from django.db.models import Q
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+            
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q= request.GET.get('q') if request.GET.get('q') != None else ''
